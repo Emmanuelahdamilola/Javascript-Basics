f1b328cb4cd565b1a9679e670113a8be172b96e3
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2025-05-01 22:11:50 +0100

    Javascript

---
+++ main.js
@@ -0,0 +1,29 @@
+// Variables Declaration in JavaScript
+var city = "New York";
+let age = 25;
+const name = "Emmanuelah";
+
+// Data Types in JavaScript
+let greeting = "Hi, Good morning!"; // String
+
+// let num = 1000;  Number
+
+let isTrue = true; // Boolean
+
+let grade = null; // Null
+
+let result;  // Undefined
+
+let user = {username: 'Emma', age: 25 }; // Object
+
+// Operators in JavaScript
+
+let addNumber = 5 + 10; // Arithmetic operators ex. addition, subtraction, division, multiplication
+
+let isLessThan = 10 < 20; // Comparison opeartors ex. equalto, less than, greater than, not equal to
+
+let score = true && false; // Logical operators ex. AND (&&), OR(||)
+
+
+// Function
+
